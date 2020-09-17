015e0e55d40e7f36ca7f6b32ff47cdb64538b9b6
ndkrebbers <n.d.krebbers@gmail.com>
2020-09-17 13:01:12 +0200

    added comments

--- Assignment_1.py
+++ Assignment_1.py
@@ -5,6 +5,12 @@ import math
 
 class tree_grow:
     def __init__(self, x, y):#, nmin, minleaf, nfeat):
+        # TODO: tree data structure for nodes to support children links
+        # TODO: test algorithm
+        # TODO: remove used feature from other elements in node list
+
+
+        # Merge the data (x) and the respective labels(y) into a numpy array and add to the nodelist
         x = np.array(x)
         y = np.expand_dims(np.transpose(np.array(y)), axis=1)
         print(np.shape(x))
@@ -12,19 +18,27 @@ class tree_grow:
         xy_combined = np.concatenate([x, y], axis=1)
         nodelist = [xy_combined]
 
+        # While there are still element left in the nodelist perform this
         while nodelist:
+            # Shuffle the nodelist to pick a random current_node each time
             rd.shuffle(nodelist)
             current_node = nodelist.pop()
-            current_impurity = self.impurity(current_node)
 
+            current_impurity = self.impurity(current_node)
             if current_impurity > 0:
+
+                # Obtain the indices of the features that are considered
                 feature_set_indices = self.draw_features(current_node[:, :-1], (np.shape(current_node)[1]-1)) # nfeat
+
+                # Obtain values of best possible split from features selected above
                 impurity, split_col, split_point = self.select_feature(current_node, feature_set_indices)
                 impurity_reduction = current_impurity - impurity # die ook ergens meegeven??
 
+                # Determine splits according to the split point
                 left_child = current_node[current_node[split_col] <= split_point]
                 right_child = current_node[current_node[split_col] > split_point]
 
+                # delete used column from children
                 left_child = np.delete(left_child, split_col, 1)
                 right_child = np.delete(right_child, split_col, 1)
 
@@ -36,16 +50,31 @@ class tree_grow:
         return p * (1 - p)
 
     def draw_features(self, x, nfeat):
+        """
+        Obtain random set of columns for possible splits
+        :param x: data with multiple columns/features
+        :param nfeat: amount of features that need to be selected
+        :return: returns set of features available for possible splits
+        """
         nr_columns = x.shape[1]
         column_indices = rd.sample(range(0, nr_columns), nfeat)
         return x[:, column_indices]
 
     def select_feature(self, current_node, feature_set_indices):
+        """
+        Decide best possible split from selected columns
+        :param current_node: Data and labels belonging to the current node
+        :param feature_set_indices: indices of the columns which will be compared to eachother
+        :return: returns values belonging to the best possible split
+        """
         x = current_node[:, :-1]
         y = current_node[:, -1]
+
         lowest_impurity = math.inf
         best_col_index = -1
         best_split_point = -1
+
+        # Calculate impurity minimization for each column and choose lowest impurity
         for col in x[:, feature_set_indices]:
             split_point, impurity = self.best_split(col, y)
             if impurity <= lowest_impurity:
@@ -61,9 +90,11 @@ class tree_grow:
         :param labels:  labels (0/1) of data
         :return:    returns data value of the best possible split position
         """
-        data, labels = zip(*sorted(zip(data, labels)))  # Sort both lists in the same order
+        # Sort both lists in the same order
+        data, labels = zip(*sorted(zip(data, labels)))
 
-        sorted_data_unique = np.array(sorted(set(data)))  # Remove duplicate values
+        # Remove duplicate values
+        sorted_data_unique = np.array(sorted(set(data)))
         labels = np.array(labels)
 
         data_len = len(sorted_data_unique)
