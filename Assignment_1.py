9762b3ee8bd51640f5f45e7091c6e14ae78eece8
ndkrebbers <n.d.krebbers@gmail.com>
2020-09-18 16:19:33 +0200

    klein beetje gebugfixed

--- Assignment_1.py
+++ Assignment_1.py
@@ -5,7 +5,6 @@ import math
 
 class tree_grow:
     def __init__(self, x, y):#, nmin, minleaf, nfeat):
-        # TODO: tree data structure for nodes to support children links
         # TODO: test algorithm
         # TODO: remove used feature from other elements in node list
 
@@ -13,10 +12,11 @@ class tree_grow:
         # Merge the data (x) and the respective labels(y) into a numpy array and add to the nodelist
         x = np.array(x)
         y = np.expand_dims(np.transpose(np.array(y)), axis=1)
-        print(np.shape(x))
-        print(np.shape(y))
+
         xy_combined = np.concatenate([x, y], axis=1)
-        nodelist = [xy_combined]
+
+        xy_node = node(xy_combined)
+        nodelist = [xy_node]
 
         # While there are still element left in the nodelist perform this
         while nodelist:
@@ -24,29 +24,44 @@ class tree_grow:
             rd.shuffle(nodelist)
             current_node = nodelist.pop()
 
-            current_impurity = self.impurity(current_node)
+            current_impurity = self.impurity(current_node.data)
             if current_impurity > 0:
 
                 # Obtain the indices of the features that are considered
-                feature_set_indices = self.draw_features(current_node[:, :-1], (np.shape(current_node)[1]-1)) # nfeat
+                feature_set_indices = self.draw_features(current_node.data[:, :-1], (np.shape(current_node.data)[1]-1)) # nfeat
 
                 # Obtain values of best possible split from features selected above
-                impurity, split_col, split_point = self.select_feature(current_node, feature_set_indices)
+                impurity, split_col, split_point = self.select_feature(current_node.data, feature_set_indices)
                 impurity_reduction = current_impurity - impurity # die ook ergens meegeven??
 
                 # Determine splits according to the split point
-                left_child = current_node[current_node[split_col] <= split_point]
-                right_child = current_node[current_node[split_col] > split_point]
+                left_child = current_node.data[current_node.data[:, split_col] <= split_point]
+                right_child = current_node.data[current_node.data[:, split_col] > split_point]
+                print(left_child[:, -1])
+                print(right_child[:, -1])
 
                 # delete used column from children
                 left_child = np.delete(left_child, split_col, 1)
                 right_child = np.delete(right_child, split_col, 1)
 
-                nodelist.append(left_child)
-                nodelist.append(right_child)
+                # Remove used column from other still to be explored nodes
+                # TODO: denk niet dat dit al goed werkt zo
+                for n in nodelist:
+                    np.delete(n.data, split_col, 1)
+
+                # Transform raw data children to node children
+                left_node = node(left_child)
+                right_node = node(right_child)
+
+                nodelist.append(left_node)
+                nodelist.append(right_node)
+
+                current_node.left_child = left_node
+                current_node.right_child = right_node
+                print()
 
     def impurity(self, current_node):
-        p = np.sum(current_node[:, -1]) / len(current_node[:, -1])
+        p = np.sum(current_node[:, -1]) / current_node[:, -1].shape[0]
         return p * (1 - p)
 
     def draw_features(self, x, nfeat):
@@ -58,7 +73,7 @@ class tree_grow:
         """
         nr_columns = x.shape[1]
         column_indices = rd.sample(range(0, nr_columns), nfeat)
-        return x[:, column_indices]
+        return column_indices
 
     def select_feature(self, current_node, feature_set_indices):
         """
@@ -75,8 +90,8 @@ class tree_grow:
         best_split_point = -1
 
         # Calculate impurity minimization for each column and choose lowest impurity
-        for col in x[:, feature_set_indices]:
-            split_point, impurity = self.best_split(col, y)
+        for col in feature_set_indices:
+            split_point, impurity = self.best_split(x[:, col], y)
             if impurity <= lowest_impurity:
                 lowest_impurity = impurity
                 best_col_index = col
@@ -115,7 +130,7 @@ class tree_grow:
             right_eq = ratio_right * q * (1 - q)
 
             current_impurity = left_eq + right_eq
-            print(round(current_impurity, 2))
+            # print(round(current_impurity, 2))
             if current_impurity <= lowest_impurity:
                 lowest_impurity = current_impurity
                 best_split_point = split
@@ -139,6 +154,13 @@ class tree_grow:
 class tree_pred:
     def __init__(self, x, tr):
         x = 0
+
+
+class node:
+    def __init__(self, x, child=None):
+        self.data = x
+        self.left_child = child
+        self.right_child = child
 
 
 example_arr = [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1, 2, 3, 4, 5]]
