9030cac1a41d0ea7dec49eec76d84badff12aee9
Zwierd <z.grotenhuis@students.uu.nl>
2020-09-29 21:34:20 +0200

    Merged random_forests.py into tree.py

--- tree.py
+++ tree.py
@@ -249,6 +249,46 @@ def tree_pred(x, tr):
 
     return y
 
+def tree_grow_b(x, y, nmin, minleaf, nfeat, m):
+    """
+    Grows a number M of trees with the parameters corresponding to tree_grow.
+    :param m: number of trees to grow.
+    :return trees: a list of trees.
+    """
+    trees = []
+    for tree in range(0, m):
+        xnew = []
+        ynew = []
+        for datapoint in x:
+            ran = rd.randint(0, len(x)-1)
+            xnew.append(x[ran])
+            ynew.append(y[ran])
+        trees.append(tree_grow(xnew, ynew, nmin, minleaf, nfeat))
+    return trees
+
+def tree_pred_b(trees, x):
+    """
+    Predicts the classification of x, given a number of trees.
+    :param trees: list of trees
+    :param x: data matrix that needs to be classified.
+    :return y: predicted classification of x.
+    """
+    predictions = []
+    for tree in trees:
+        predictions.append(tree_pred(x, tree.tree))
+
+    y = []
+    for column in range(0, len(predictions[0].y)):
+        count = 0
+        for row in range(0,len(predictions)):
+            count += predictions[row].y[column]
+
+        if float(count)/len(predictions[0].y) > 0.5:
+            y.append(1)
+        else:
+            y.append(0)
+
+    return y
 
 class node:
     def __init__(self, x, dep, larger_than_bool, child=None, col=None, val=None):
