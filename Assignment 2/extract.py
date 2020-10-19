c51029eb60586bf2cc2fd8318c796475d9b9e29c
Zwierd <z.grotenhuis@students.uu.nl>
2020-10-19 22:50:04 +0200

    text cleaning

--- Assignment 2/extract.py
+++ Assignment 2/extract.py
@@ -1,7 +1,11 @@
 import numpy as np
 import os
+import nltk
+import string
+import re
+nltk.download('stopwords')
 
-
+nltk.download('punkt')
 def extract_all():
     deceptive_train, deceptive_test = extract(False)
     truthful_train, truthful_test = extract(True)
@@ -22,7 +26,10 @@ def extract(legit):
         else:
             train_data = train_data + read_fold(path, fold, legit)
     train_data = np.array(train_data, dtype=object)
-
+    test_data = np.array(test_data, dtype=object)
+    train_data = clean_strings(train_data)
+    test_data = clean_strings(test_data)
+    print(test_data)
     return train_data, test_data
 
 
@@ -34,3 +41,17 @@ def read_fold(path, fold, legit):
             data.append([file_text, int(legit), fold])
 
     return data
+
+def clean_strings(data):
+
+    for row in data:
+        row[0][0]=(row[0][0].lower())
+        stopwords = nltk.corpus.stopwords.words('english')
+        #exclude = set(string.punctuation)
+
+        filtered = [w for w in row[0][0].split() if w not in stopwords]# and w not in exclude]
+        tokens = nltk.tokenize.word_tokenize(" ".join(filtered))
+        noPunctuation = re.sub(r'[^\w\s]', ''," ".join(tokens) )
+        row[0][0] = " ".join(noPunctuation.split())
+    return data
+extract_all()
